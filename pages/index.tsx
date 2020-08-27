aeb3a22e49c1307adc019612b0be11ee54f68bb7
Matheus Pires <matheushenriquepires99@gmail.com>
2020-08-26 21:52:12 -0300

    initial commit

---
+++ pages/index.tsx
@@ -0,0 +1,14 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Next.js Conf</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <h1>Hello World</h1>
+    </div>
+  )
+}
