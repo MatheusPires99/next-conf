e05c4357b45fb2ea0aee829db0e985763efddde2
Matheus Pires <matheushenriquepires99@gmail.com>
2020-08-27 08:31:58 -0300

    feat: create styled-components config

---
+++ src/styles/global.ts
@@ -0,0 +1,40 @@
+import { createGlobalStyle } from 'styled-components';
+
+export default createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    outline: 0;
+    box-sizing: border-box
+  }
+
+  *:focus {
+    outline: 0;
+  }
+
+  html, body, #root {
+    height: 100%;
+  }
+
+  body {
+    background-color: ${({ theme }) => theme.colors.background};
+    color: ${({ theme }) => theme.colors.primary};
+    -webkit-font-smoothing: antialiased;
+  }
+
+  body, input, button {
+    font: 400 16px Inter, sans-serif;
+  }
+
+  button {
+    cursor: pointer;
+  }
+
+  a {
+    text-decoration: none;
+  }
+
+  ul {
+    list-style: none;
+  }
+`;
---
+++ src/styles/theme.ts
@@ -0,0 +1,9 @@
+const theme = {
+  colors: {
+    background: '#000',
+    primary: '#FFF',
+    secundary: '#8A8F98',
+  },
+};
+
+export default theme;
